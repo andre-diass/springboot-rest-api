98c5f291f6781ae12c775d6ef4e3613cc80ce3fb
andre-diass <adsf@poli.br>
2023-11-08 09:46:28 -0300

    feat: set up DB for tests

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,28 @@
+package med.voll.api.domain.medico;
+
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.endereco.DadosEndereco;
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+    }
+}
